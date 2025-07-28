8a9c0e1ae1cfcbdb47ed5b5497a6e7e56a09d223
Karthi V <vskarthi114@gmail.com>
2025-07-28 23:26:19 +0530

    Delete old Conditional pattern programs

---
+++ src/com/gqt/corejava/pattern_programs_withconditional_statements/ConditionalPattern_Alphabet_Q.java
@@ -0,0 +1,52 @@
+/**
+
+# # # # # # # # # #   
+#                 #   
+#                 #   
+#                 #   
+#                 #   
+#         #       #   
+#           #     #   
+#             #   #   
+#               # #   
+# # # # # # # # # #   
+                    # 
+                    
+ */
+package com.gqt.corejava.pattern_programs_withconditional_statements;
+
+import java.util.Scanner;
+
+/**
+ * @author karthi v
+ * @category Patterns
+ * @description This is an example for conditional pattern - alphabet Q
+ */
+public class ConditionalPattern_Alphabet_Q {
+
+	/**
+	 * @param args
+	 * @description This contains source code for the pattern
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the size : ");
+		int n = sc.nextInt();
+		sc.close();
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(j==0 && i<n-1|| i==0  && j<(n-1)||
+						j==(n-2)&& i<(n-2) || i==(n-2) && j<(n-2) 
+						|| i==j && j>=(n/2)) 
+				{
+					System.out.print("# ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+		System.out.println();
+		}
+	}
+
+}
---
+++ src/com/gqt/corejava/pattern_programs_withconditional_statements/ConditionalPattern_Alphabet_R.java
@@ -0,0 +1,60 @@
+/**
+
+# # # # # #           
+#         #           
+#         #           
+#         #           
+#         #           
+# # # # # #           
+# #                   
+#   #                 
+#     #               
+#       #             
+#         #           
+
+ */
+package com.gqt.corejava.pattern_programs_withconditional_statements;
+
+import java.util.Scanner;
+
+/**
+ * @author karthi v
+ * @category Patterns
+ * @description This is an example for conditional pattern - alphabet R
+ */
+public class ConditionalPattern_Alphabet_R {
+
+	/**
+	 * @param args
+	 * @description This contains source code for the pattern
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the size : ");
+		int n = sc.nextInt();
+		sc.close();
+		for(int i=0;i<n;i++) {
+//			for(int j=0;j<n;j++) {
+//				if(j==0 ||  i==0 && j<=(n/2) || j==(n/2) && i<=(n/2)||
+//						i==(n/2) && j<=(n/2) || i-j ==(n/2) ) 
+//				{
+//					System.out.print("# ");
+//				}
+//				else {
+//					System.out.print("  ");
+//				}
+//			}
+			
+			for (int j = 0; j < n; j++) {
+				if (j == 0 || i == 0 || j == (n - 1) && i <= (n / 2) 
+						|| i == (n / 2) || i - j == (n / 2)) {
+					System.out.print("# ");
+				} else {
+					System.out.print("  ");
+				}
+			}
+		System.out.println();
+		}
+	}
+
+}
